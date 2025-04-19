1ff9c60a1c0ebf762f10c9584b5a42820839fd66
Luis Ramos <luisenriqueramos1977@gmail.com>
2025-04-19 08:01:54 +0200

    frontend app

--- backend/project/project/settings.py
+++ backend/project/project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-h(p65afe(^u2$v3)-1a9z+lt-4cpbkdn$$%0v#42m6hi2vxsem
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
 ]
+
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -47,6 +50,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
+
 ]
 
 ROOT_URLCONF = 'project.urls'
@@ -120,3 +125,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# this should be put at the end of the settings.py file
+CORS_ORIGIN_WHITELIST = [
+     'http://localhost:3000'
+]
